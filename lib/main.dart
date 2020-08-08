6b4c4de90c362fec857171b0e9e5a570acb0da12
bb10789 <chakhim.kam@gmail.com>
2020-08-08 01:20:33 -0700

    Login Page Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:med_id/pages/GetStarted.dart';
 import "package:med_id/pages/Intro.dart";
+import 'package:med_id/pages/LoginScreen.dart';
 
 void main() =>
     runApp(MaterialApp(
+      initialRoute: "/loginscreen",
       routes: {
-          "/": (context) => GetStarted(),
+          "/": (context) => Intro(),
+          "/getstarted": (context) => GetStarted(),
+        "/loginscreen": (context) => LoginScreen(),
+
       }
     ));
