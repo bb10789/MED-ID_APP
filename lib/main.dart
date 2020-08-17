57defe4aca8dab008ae7568af17e4ec393111e3f
Willjay90 <willjay90.t@gmail.com>
2020-08-17 15:54:11 -0700

    prototype pairingView.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,11 @@ import "package:med_id/pages/Intro.dart";
 import 'package:med_id/pages/LoginScreen.dart';
 import "package:med_id/pages/Finding.dart";
 
-void main() =>
-    runApp(MaterialApp(
-      initialRoute: "/",
-      routes: {
-          "/": (context) => Intro(),
-          "/getstarted": (context) => GetStarted(),
-        "/loginscreen": (context) => LoginScreen(),
-        "/findingscreen": (context) => Finding(),
+import 'pages/PairingView.dart';
 
-      }
-    ));
+void main() => runApp(MaterialApp(initialRoute: "/", routes: {
+      "/": (context) => PairingView(),
+      "/getstarted": (context) => GetStarted(),
+      "/loginscreen": (context) => LoginScreen(),
+      "/findingscreen": (context) => Finding(),
+    }));
