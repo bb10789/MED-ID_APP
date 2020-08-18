289978607fdc3bb34498734ba15b3f445898907f
Willjay90 <willjay90.t@gmail.com>
2020-08-18 10:31:16 -0700

    keyboard positioning

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,10 @@ import 'package:med_id/pages/LoginScreen.dart';
 import "package:med_id/pages/Finding.dart";
 import "package:med_id/pages/NameReg.dart";
 
-<<<<<<< HEAD
-import 'pages/PairingView.dart';
-
-void main() => runApp(MaterialApp(initialRoute: "/", routes: {
-      "/": (context) => PairingView(),
+void main() => runApp(MaterialApp(initialRoute: "/nameRegscreen", routes: {
+      "/": (context) => Intro(),
       "/getstarted": (context) => GetStarted(),
       "/loginscreen": (context) => LoginScreen(),
       "/findingscreen": (context) => Finding(),
+      "/nameRegscreen": (context) => NameReg(),
     }));
-=======
-void main() =>
-    runApp(MaterialApp(
-      initialRoute: "/nameRegscreen",
-      routes: {
-          "/": (context) => Intro(),
-          "/getstarted": (context) => GetStarted(),
-        "/loginscreen": (context) => LoginScreen(),
-        "/findingscreen": (context) => Finding(),
-        "/nameRegscreen" : (context) => NameReg(),
-      }
-    ));
->>>>>>> f690e20bbe9df4ab41b76b8cde01a20a9b4308b9
--- lib/pages/NameReg.dart
+++ lib/pages/NameReg.dart
@@ -1,123 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hexcolor/hexcolor.dart';
 import "package:med_id/utilities/constants.dart";
 
-class NameReg extends StatefulWidget {
-  @override
-  _NameRegState createState() => _NameRegState();
-}
+class NameReg extends StatelessWidget {
+  Widget _firstNameTextField() {
+    return TextFormField(
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          color: Hexcolor("#000000"),
+          fontFamily: "OpenSans",
+        ),
+        decoration: InputDecoration(
+            border: mOutlineInputStyle,
+            contentPadding: EdgeInsets.only(left: 40, top: 20, bottom: 20),
+            hintText: "First Name",
+            hintStyle: TextStyle(fontSize: 18, color: Hexcolor("#38000000")),
+            fillColor: Colors.white,
+            filled: true));
+  }
 
-class _NameRegState extends State<NameReg> {
+  Widget _lastNameTextField() {
+    return TextFormField(
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          color: Hexcolor("#000000"),
+          fontFamily: "OpenSans",
+        ),
+        decoration: InputDecoration(
+            border: mOutlineInputStyle,
+            contentPadding: EdgeInsets.only(left: 40, top: 20, bottom: 20),
+            hintText: "Last Name",
+            hintStyle: TextStyle(fontSize: 18, color: Hexcolor("#38000000")),
+            fillColor: Colors.white,
+            filled: true));
+  }
 
-  Widget _buildFirstNameTF() {
-  return  Column(
-      children: <Widget>[
-        Container(
-            alignment: Alignment.centerLeft,
-            height: 60,
-            decoration: mBoxDecorationStyle,
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              style: TextStyle(
-                color: Hexcolor("#000000"),
-                fontFamily: "OpenSans",
-              ),
-              textAlign: TextAlign.left,
-              decoration: InputDecoration(
-                border: mOutlineInputStyle,
-                contentPadding: EdgeInsets.only(left: 40, top: 20, bottom: 20),
-                hintText: "First Name",
-                hintStyle: TextStyle(
-                    fontSize: 18,
-                    color: Hexcolor("#38000000")),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-            ))
-      ],
+  Widget _textFields() {
+    return FractionalTranslation(
+      translation: Offset(0.0, 0.25),
+      child: Column(
+        children: [
+          _firstNameTextField(),
+          SizedBox(height: 25),
+          _lastNameTextField()
+        ],
+      ),
     );
   }
 
-  Widget _buildLastNameTF() {
-    return Column(
-      children: <Widget>[
-        SizedBox(height: 25),
-        Container(
-            alignment: Alignment.centerLeft,
-            height: 60,
-            decoration: mBoxDecorationStyle,
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              style: TextStyle(
-                color: Hexcolor("#000000"),
-                fontFamily: "OpenSans",
-              ),
-              textAlign: TextAlign.left,
-              decoration: InputDecoration(
-                border: mOutlineInputStyle,
-                contentPadding: EdgeInsets.only(left: 40, top: 20, bottom: 20),
-                hintText: "Last Name",
-                hintStyle: TextStyle(
-                    fontSize: 18,
-                    color: Hexcolor("#38000000")),
-                fillColor: Colors.white,
-                filled: true,
-              ),
-            ))
-      ],
+  Widget _nextButton(context) {
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(new FocusNode());
+              Navigator.pushNamed(context, "/findingscreen");
+            },
+            child: Container(
+              height: 40, // TODO: Calculate propotional size
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+              alignment: Alignment.center,
+              child: Text("NEXT",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: "OpenSans",
+                      letterSpacing: 2,
+                      color: Colors.white)),
+            )),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: AnnotatedRegion<SystemUiOverlayStyle>(
-            value: SystemUiOverlayStyle.light,
-            child: GestureDetector(
-              onTap: () => FocusScope.of(context).unfocus(),
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                      color: Hexcolor("FFFFFF"),
-                      child: SingleChildScrollView(
-                          physics: AlwaysScrollableScrollPhysics(),
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 40.0, vertical: 200.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              _buildFirstNameTF(),
-                              _buildLastNameTF(),
-                              SizedBox(height: 250),
-                              Center(
-                                child: FlatButton(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(30),
-                                      side: BorderSide(
-                                        color: Colors.blue,
-                                        width: 1,
-                                      ),
-                                    ),
-                                    color: Colors.blue,
-                                    textColor: Colors.white,
-                                    onPressed: () {
-                                      Navigator.pushNamed(context, "/findingscreen");
-                                    },
-                                    padding: EdgeInsets.fromLTRB(140, 20, 140, 20),
-                                    child: Text("NEXT",
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          fontFamily: "OpenSans",
-                                          letterSpacing: 2,
-                                        ))),
-                              ),
-                            ],
-                          )))
-                ],
-              ),
-            )));
+    Widget _stackView = Stack(
+      children: [_textFields(), _nextButton(context)],
+    );
+
+    return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: Scaffold(
+            body: Container(
+          padding: EdgeInsets.all(16),
+          child: _stackView,
+        )));
   }
 }
